11d7ba32a50c92b2984e2c6a539ce2ac8082e67a
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-09-01 18:34:52 +0500

    late

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -56,7 +56,7 @@ const Home = () => {
                     <Card className="h-100 shadow-sm">
                       <div style={{ width: "100%", height: "300px", backgroundColor: "#f8f9fa", display: "flex", alignItems: "center", justifyContent: "center" }}>
                         <Card.Img
-                          src={`https://alqemahgoldsmith.com${img.url}`}
+                          src={`https://alqemahgoldsmith.com/${img.url}`}
                           alt={`Project ${index + 1}`}
                           style={{
                             maxWidth: "100%",

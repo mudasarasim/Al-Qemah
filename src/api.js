936a76f2cb36b9594fd5e1e5ca3426a7d8c9394d
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-09-01 19:34:28 +0500

    dj

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://alqemahgoldsmith.com/api",
 });
 
 // Token automatically attach karega agar login hua hai
